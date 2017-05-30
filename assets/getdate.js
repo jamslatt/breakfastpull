bf1a9df582416a315154a3114b313a16dbe70e86
James Slattery <james@slattery.tech>
2017-05-30 10:47:44 -0700

    Fix getdate()

--- assets/getdate.js
+++ assets/getdate.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    var now = new Date();
     var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     var day = days[now.getDay()];
     console.log(day);
